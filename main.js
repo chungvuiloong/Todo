1a70419e15af40393485a9cb1b60d31b92d3c2cc
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-03-05 12:47:04 +0200

    testing

--- main.js
+++ main.js
@@ -2,4 +2,8 @@ let text = document.querySelector("#text");
 let button = document.querySelector("#add");
 let createLi = document.createElement("li");
 
-button.addEventListener("click", () => console.log("clicked"));
+testing = () => {
+  console.log("Testing");
+};
+
+// button.addEventListener("click", () => console.log("clicked"));
