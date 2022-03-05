4c8be386ca61c0f57e5379ce3d447d265634d52f
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-03-05 19:19:31 +0200

    my to do list javscript is done!!! left css

--- main.js
+++ main.js
@@ -3,16 +3,38 @@ let button = document.querySelector("#add");
 let deleteAll = document.querySelector("#deleteAll");
 let list = document.querySelector("#checklist");
 
+////////////////////////////////
+
+let i = 0;
+
 testing = (event) => {
   event.preventDefault();
+
   if (text.value == "") {
     console.log("Please enter something.");
   } else {
-    list.insertAdjacentHTML("beforeend", `<li>${text.value}</li>`);
+    list.insertAdjacentHTML(
+      "beforeend",
+      `<li>${text.value} <button type="button" class="delete" onclick="this.parentNode.parentNode.removeChild(this.parentNode);">X</button></li>`
+    );
+
+    //  console.log(list.children); // shows the list of li   checklist.children[0], [1], [2]
+    //console.log(list.children.length); //shows number of li in checklist
+    //console.log(list.childElementCount); //shows number of li in checklist
+
+    //This resets the value in the placeholder
     text.value = "";
   }
 };
 
+// version 1
+// remove = () => {
+//   for (let index = 0; index <= list.children.length; index++) {
+//     list.children[index].remove();
+//   }
+// };
+
 button.addEventListener("click", testing);
-deleteAll.addEventListener("click", () => list.remove(););
-// deletedButton.addEventListener("click", delete);
+
+// Deletes everything and cant add things after this
+deleteAll.addEventListener("click", () => list.remove());
