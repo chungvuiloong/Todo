08369c1f0031acab2cfeedaa28058e6f9aedefe7
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-03-05 12:43:34 +0200

    commmit

---
+++ main.js
@@ -0,0 +1,5 @@
+let text = document.querySelector("#text");
+let button = document.querySelector("#add");
+let createLi = document.createElement("li");
+
+button.addEventListener("click", () => console.log("clicked"));
